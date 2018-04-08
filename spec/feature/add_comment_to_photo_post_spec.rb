772c3c078af2ade8f97896587082565875505992
cgibsonmm <cgibsonmm@gmail.com>
2018-04-08 17:20:19 -0400

    started work on adding comments: failing

---
+++ spec/feature/add_comment_to_photo_post_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature 'Adding comments to photo_posts' do
+  before do
+    @user = create(:user)
+    user2 = create(:user, email: 'test@emaill.com',
+                          user_name: 'testuser2', id: 2)
+    post = create(:photo_post, user_id: 2)
+  end
+
+  context 'when user signed in' do
+    before do
+      sign_in_with @user
+      visit photo_posts_path
+    end
+
+    scenario 'user can add a comment to a post' do
+      expect(page).to have_content('MyString')
+      fill_in 'Add Comment', with: 'This photo is great!'
+      click_button 'Submit'
+
+      expect(page).to have_content('This photo is great!')
+    end
+  end
+end
