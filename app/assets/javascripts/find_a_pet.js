bf9186494d6fc1a0980f83ea2e54e4a6936a7ec7
cgibsonmm <cgibsonmm@gmail.com>
2018-06-07 16:48:42 -0400

    api return working

--- app/assets/javascripts/find_a_pet.js
+++ app/assets/javascripts/find_a_pet.js
@@ -4,36 +4,39 @@
 
 
 $(document).ready(function(){
-    var url = 'http://api.petfinder.com/pet.getRandom?key=34cddab0b6464620f91ee1a0b406bfee&shelterid=KY305&output=full&format=json';
+  // Zip code input
+  var zipInput = document.getElementById('zipInput')
+  var speciesType = document.getElementById('speciesType')
+
+  // on click clears any previous search and requests
+  $('#search_button').click(function() {
+    $('#petsList').empty()
+    var species = speciesType.value
+    var urlWithZip = 'http://api.petfinder.com/pet.find?animal='+ species +'&location=' + zipInput.value + '&key=34cddab0b6464620f91ee1a0b406bfee&&output=full&format=json'
+
+    if (zipInput.value.length < 5){
+      alert('zip code must be present')
+    }
+
     $.ajax({
-        type : 'GET',
-        data : {},
-        url : url+'&callback=?' ,
-        dataType: 'json',
-        success : function(data) {
-            // stores result
-            var result = '';
+      url: urlWithZip,
+      type: 'GET',
+      dataType: 'jsonp'
+    })
+    .done(function(data){
+      var returnedPets = data.petfinder.pets.pet
+      returnedPets.forEach(function(pet){
+        var petName = Object.values(pet.name)
+        var petImage =  (Object.values(pet.media.photos.photo[2])[1])
 
-            var petfinder = data.petfinder;
-            var infoHTML = '<ul>';
-            infoHTML += '<li>';
-            infoHTML += '<strong>Description</strong><br>';
-            infoHTML += petfinder.pet.description['$t'];
-            infoHTML += '</li>';
-
-            infoHTML += '</li>';
-
-            infoHTML += '</ul>';
-            // return infoHTML;
-            $('#petfinderInfo').html(infoHTML);
-
-            // $('#petfinderInfo').html(petfinder.pet.description['$t']);
-            //
-            console.log(petfinder);
-        },
-        error : function(request,error)
-        {
-            alert("Request: "+JSON.stringify(request));
-        }
-    });
+        $('#petsList').append(
+          "<li class='pet'>" + petName + "</li>",
+          '<img src='+ petImage + '>'
+        )
+      })
+    })
+    .error(function() {
+      alert('Error grabing response from server')
+    })
+  })
 });
