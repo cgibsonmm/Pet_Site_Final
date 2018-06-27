3dbd295aa68e73a908a93b21abd4ddb29b16b4e8
cgibsonmm <cgibsonmm@gmail.com>
2018-06-27 13:23:08 -0400

    not validating

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
+
 //= require popper
 //= require bootstrap-sprockets
 //= require jquery.jcrop
--- app/controllers/shot_records_controller.rb
+++ app/controllers/shot_records_controller.rb
@@ -1,32 +1,13 @@
 class ShotRecordsController < ApplicationController
   before_action :set_animal
 
-
-  # May use this
-  # def new
-  #   respond_to do |format|
-  #     format.html { render :new}
-  #     format.js
-  #   end
-  # end
-
   def create
     @shot_record = @animal.shot_records.build(record_params)
-    if @shot_record.validate(params[:name])
-      @shot_record.save
-      flash[:success] = 'Successfully created Shot Record'
+    if @shot_record.save
       respond_to do |format|
-        format.html { redirect_to user_animals_path(current_user)}
         format.js
       end
-    else
-      respond_to do |format|
-        format.html { render :new }
-        format.json { render json: @shot_record.errors.full_messages, status: :unprocessable_entity }
-        format.js   { render layout: false, content_type: 'text/javascript'}
-      end
     end
-
   end
 
 
